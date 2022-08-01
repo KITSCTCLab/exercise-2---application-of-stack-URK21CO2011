afd78e4b874a503afd61f94bb1a25ed058b1d552
URK21CO2011 <110015075+URK21CO2011@users.noreply.github.com>
2022-08-01 21:01:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 class Evaluate:
-	  """This class validates and evaluate postfix expression.
+		  """This class validates and evaluate postfix expression.
 	  Attributes:
 	      top: An integer which denotes the index of the element at the top of the stack currently.
 	      size_of_stack: An integer which represents the size of stack.
@@ -10,12 +10,12 @@ class Evaluate:
 
 	
 
-         def __init__(self, size):
+	  def __init__(self, size):
 	    """Inits Evaluate with top, size_of_stack and stack.
 	    Arguments:
 	      size_of_stack: An integer to set the size of stack.
 	    """
-             self.top = -1
+	    self.top = -1
 	    self.size_of_stack = size
 	    self.stack = []
 	
@@ -118,16 +118,16 @@ class Evaluate:
 	    
 	    return int(self.stack[0])
 	
-
-	
-
-	# Do not change the following code
-	postfix_expression = input()  # Read postfix expression
-	tokens = postfix_expression.split()
-	evaluate = Evaluate(len(tokens))
-	if evaluate.validate_postfix_expression(tokens):
-	    print(evaluate.evaluate_postfix_expression(tokens))
-	else:
-	    print('Invalid postfix expression')
+# Do not change the following code
+postfix_expression = input()  # Read postfix expression
+tokens = postfix_expression.split()
+evaluate = Evaluate(len(tokens))
+if evaluate.validate_postfix_expression(tokens):
+    print(evaluate.evaluate_postfix_expression(tokens))
+else:
+    print('Invalid postfix expression')
 
 
+
+
+	  
